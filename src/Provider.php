dd6b78ec1e738fb91fffdcd8354e3bc8f20a7224
vocolboy <vocolboy@gmail.com>
2016-06-06 01:23:35 +0800

    Fix Call to undefined method getAccessToken

--- src/Provider.php
+++ src/Provider.php
@@ -77,16 +77,4 @@ class Provider extends AbstractProvider implements ProviderInterface
             'grant_type' => 'authorization_code',
         ]);
     }
-
-    /**
-     * {@inheritdoc}
-     */
-    public function user()
-    {
-        $user = $this->mapUserToObject($this->getUserByToken(
-            $token = $this->getAccessToken($this->getCode())
-        ));
-
-        return $user->setToken($token);
-    }
 }
